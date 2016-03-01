5509e91d9ff690088feab17a96159402a74ec3e6
Anderson banihirwe <axbanihirwe@ualr.edu>
2016-03-01 15:00:27 -0600

    It's Turtle's Day!
    Some fun Python Graphics programs

---
+++ Turtle for Fun/Amazing Python.py
@@ -0,0 +1,38 @@
+import turtle
+import random
+
+
+def main():
+    tList = []
+    head = 0
+    numTurtles = 10
+    wn = turtle.Screen()
+    wn.setup(500, 500)
+    for i in range(numTurtles):
+        nt = turtle.Turtle()  # make a new turtle, initialize values
+        nt.setheading(head)
+        nt.pensize(2)
+        nt.color(random.randrange(255), random.randrange(255), random.randrange(255))
+        nt.speed(10)
+        wn.tracer(30, 0)
+        tList.append(nt)  # Add the new turtle to the list
+        head += 360 / numTurtles
+
+    for i in range(100):
+        moveTurtles(tList, 15, i)
+
+    w = tList[0]
+    w.up()
+    #w.goto(0, 40)
+    #w.write("Welcome ", True, "center", "40pt Bold")
+    #w.goto(0, -35)
+    #w.write("World of Python ", True, "center", "20pt Bold")
+
+
+def moveTurtles(turtleList, dist, angle):
+    for turtle in turtleList:
+        # Make every turtle on the list do the same actions
+        turtle.forward(dist)
+        turtle.right(angle)
+
+main()
---
+++ Turtle for Fun/myturtle.py
@@ -0,0 +1,8 @@
+import turtle
+
+wn = turtle.Screen()
+alexa = turtle.Turtle()
+alexa.forward(150)
+alexa.left(90)
+alexa.forward(75)
+
