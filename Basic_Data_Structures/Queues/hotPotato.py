5962e5b91e89b56013a465d8b3756982effc2ae0
andersy005 <axbanihirwe@ualr.edu>
2016-03-11 00:00:17 -0600

    Hot Potato

---
+++ Basic_Data_Structures/Queues/hotPotato.py
@@ -0,0 +1,18 @@
+from pythonds.basic.queue import Queue
+
+def hotPotato(nameList, num):
+	simqueue = Queue()
+
+	for name in nameList:
+		simqueue.enqueue(name)
+
+	while simqueue.size() > 1:
+		for i in range (num):
+			simqueue.enqueue(simqueue.dequeue())
+
+
+		simqueue.dequeue()
+
+	return simqueue.dequeue()
+
+print(hotPotato(["Bill","David","Susan","Jane","Kent","Brad"],7))
