16336eeb838df221fa7a2edc8d8d6054ddd059f6
andersy005 <axbanihirwe@ualr.edu>
2016-03-12 09:05:04 -0600

    Deque ADT: front and rear Queue

---
+++ Basic_Data_Structures/Deque/Deque_impl.py
@@ -0,0 +1,25 @@
+class Deque:
+	"""docstring for Deque"""
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def addFront(self, item):
+		self.items.append(item)
+
+	def addRear(self, item):
+		self.items.insert(0,item)
+
+	def RemoveFront(self):
+		return self.items.pop()
+
+	def RemoveRear(self):
+		return self.items.pop(0)
+
+	def size(self):
+		return len(self.items)
+
+		
+		
