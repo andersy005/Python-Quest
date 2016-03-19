d0471204625e79bd4b649b40f7adbcf55ce3d0e4
andersy005 <axbanihirwe@ualr.edu>
2016-03-19 13:38:29 -0500

    indentation changed

--- Sorting&Searching/sequentialSearch.py
+++ Sorting&Searching/sequentialSearch.py
@@ -1,16 +1,16 @@
 def sequentialSearch(alist, item):
-	pos = 0 
-	found = False
+    pos = 0 
+    found = False
 
-	while pos < len(alist) and not found:
-		if alist[pos] == item:
-			found = True
+    while pos < len(alist) and not found:
+        if alist[pos] == item:
+            found = True
 
-		else:
-			pos = pos + 1
+        else:
+            pos = pos + 1
 
 
-	return found
+    return found
 
 testlist =[1,15,18,16,5,7,8,83,20,14,57]
 print(sequentialSearch(testlist,3))
