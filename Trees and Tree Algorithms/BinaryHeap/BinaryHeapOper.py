88c02e55a61be7771e0078f448409705332c975c
andersy005 <axbanihirwe@ualr.edu>
2016-03-31 15:33:23 -0500

    Some BinaryHeap Operations

---
+++ Trees and Tree Algorithms/BinaryHeap/BinaryHeapOper.py
@@ -0,0 +1,14 @@
+from pythonds.trees.binheap import BinHeap 
+
+bh = BinHeap()
+bh.insert(5)
+bh.insert(55)
+bh.insert(10)
+bh.insert(2)
+bh.insert(8)
+
+print(bh.delMin())
+print(bh.delMin())
+print(bh.delMin())
+print(bh.delMin())
+print(bh.delMin())
