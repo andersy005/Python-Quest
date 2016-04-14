8ef169a412e56eb4a67bc5d165b0795951eab4a6
andersy005 <axbanihirwe@ualr.edu>
2016-04-14 17:09:49 -0500

    adds Depth First Search

--- Graphs and Graph Algorithms/Knight’sTourGraph.py
+++ Graphs and Graph Algorithms/Knight’sTourGraph.py
@@ -1,4 +1,4 @@
-from pythonds.graphs import Graph 
+from pythonds.graphs import Graph, Vertex
 
 def knightGraph(bdsize):
 	ktGraph = GrNoaph()
@@ -35,3 +35,27 @@ def legalCoord(x,bdsize):
 
 	else:
 		return False
+
+
+def knightTour(n,path, u, limit):
+	u.setColor('gray')
+	path.append(u)
+	if n < limit:
+		nbrList = list(u.getConnections())
+		i = 0
+		done = False
+		while i < len(nbrList) and not done:
+			if nbrList[i].getColor() == 'white':
+				done = knightTour(n+1, path, nbrList[i], limit)
+
+			i = i + 1
+
+		if not done: #prepare to backtrack
+			path.pop()
+			u.setColor('white')
+
+		else:
+			done = True
+
+		return done
+		
