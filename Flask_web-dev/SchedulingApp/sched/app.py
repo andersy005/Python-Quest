fda77ab2a7c81ab4282c97a8b11b21540ed6abec
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-07-24 00:50:35 +0200

    install dependencies and set different routes

--- Flask_web-dev/SchedulingApp/sched/app.py
+++ Flask_web-dev/SchedulingApp/sched/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import url_for
 
 
 app = Flask(__name__)
@@ -10,3 +11,32 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/appointments')
+def appointment_list():
+    return 'Listing of all appointments we have.'
+
+
+@app.route('/appointments/<int:appointment_id>/')
+def appointment_detail(appointment_id):
+    return 'Detail of appointment   #{}.'.format(appointment_id)
+
+
+@app.route('/appointments/<int:appointment_id>/edit', methods=['GET', 'POST'])
+def appointment_edit(appointment_id):
+    return 'Form to edit appointment #.'.format(appointment_id)
+
+
+@app.route('/appointments/<int:appointment_id>/create', methods=['GET', 'POST'])
+def appointment_create():
+    return 'Form to create a new appointment.'
+
+
+@app.route('/appointments/<int:appointment_id>/delete/', methods=['DELETE'])
+def appointment_delete(appointment_id):
+    raise NotImplementedError('DELETE')
+
+
+
+
