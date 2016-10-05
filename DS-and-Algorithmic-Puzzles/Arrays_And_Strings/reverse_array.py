23aa30145d761b685056daadc111a7dce8bddfb3
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-04 20:47:48 -0500

    Reverse an array

---
+++ DS-and-Algorithmic-Puzzles/Arrays_And_Strings/reverse_array.py
@@ -0,0 +1,16 @@
+def reverse(arr):
+
+    i = 0
+    j = len(arr) - 1
+
+    while(i < j):
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+
+
+    return arr
+
+
+print(reverse([1,2,3]))
+    
