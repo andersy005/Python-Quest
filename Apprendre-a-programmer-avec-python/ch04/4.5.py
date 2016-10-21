b50abeb170c7891c615e3f0e9e9617559164d858
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-21 01:05:16 -0500

    :tada: 4.5

---
+++ Apprendre-a-programmer-avec-python/ch04/4.5.py
@@ -0,0 +1,12 @@
+# Programme qui calcule le volume d'un parallelepipe rectangle
+
+def volume(long, height, width):
+
+    vol = long * height * width
+
+    return vol
+
+
+
+print'The volume of the parallelipiped is:', volume(10,5,6)
+
