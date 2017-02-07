20f49e2ae95d37d58b6b2a2583dfeaffd6bc24c7
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-02-07 09:25:31 -0600

    Extending Builtins

--- Python3 OOP/3-When-Objects-are-alike/messenger.py
+++ Python3 OOP/3-When-Objects-are-alike/messenger.py
@@ -1,5 +1,17 @@
+class ContactList(list):
+    def search(self, name):
+        '''Return all contacts that contain the search value in their name.'''
+
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
+
+
+
 class Contact:
-    all_contacts = []     # Class Variable, it's shared by all instances of this class
+    all_contacts = ContactList()     # Class Variable, it's shared by all instances of this class
 
 
     def __init__(self, name, email):
@@ -10,3 +22,6 @@ class Contact:
 class Supplier(Contact):
     def order(self, order):
         print("If this were a real system we would send ", "{} order to {}".format(order, self.name))
+
+
+
