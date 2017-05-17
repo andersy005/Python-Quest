f1853d9ef53528ec2e02a6c5dbd4d3b4f379e1ed
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-05-16 21:10:52 -0500

    Handling Multiple files

---
+++ CLI-Programming/readings_02.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np 
+
+def main():
+    script = sys.argv[0]
+    filename = sys.argv[1]
+    data = np.loadtxt(filename, delimiter=',')
+    for m in np.mean(data, axis=1):
+        print(m)
+
+if __name__ == '__main__':
+    main()
---
+++ CLI-Programming/readings_03.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np 
+
+def main():
+    script = sys.argv[0]
+    for filename in sys.argv[1:]:
+        data = np.loadtxt(filename, delimiter=',')
+        for m in np.mean(data, axis=1):
+            print(m)
+   
+
+if __name__ == '__main__':
+    main()
